ecef942212a8e9badff0c0455d3a01f5f4c2450f
Prajwayne <46771166+Prajwayne@users.noreply.github.com>
2023-01-31 07:41:20 +0000

    Type Texts and Load AI Answers

---
+++ cHAT GPT/client/script.js
@@ -0,0 +1,43 @@
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
+
+// not workign with react at this stage therefore have to target html elements manually
+const form = document.querySelector('form');
+const chatConatiner = document.querySelector('chat_containe');// same name as html file 
+
+// ceate one variable 
+let loadInterval;
+
+
+//function that loads our messages 
+// it will generate 3 dots while thinking (...) this function enable that 
+function loader(element)
+{
+  element.textContent = '';
+
+
+  loadInterval = setInterval(()=>{
+    element.textContent += '.';
+
+    //not more than 3 dots at 300 milisecods interval 
+    if (element.textContent === '....'){
+      element.textContent = '';
+
+    }
+  },300)
+}
+
+//typing personality letter by letter 
+function typeText(element, text){
+  let index = 0;//at the start index is set to zero
+
+  let interval = setInterval(() =>{
+    if(index <text.length){ // check if we are still typing
+      element.innerHTML += text.chartAt(index);// charecter under specific index
+      index++;
+
+    }else{
+      clearInterval(interval);// clear the inteval after reaching the end
+    }
+  }, 20)
+}
